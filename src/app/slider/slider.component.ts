ed461fbb6102a03c81a75f8b9f1791930e12da92
Bork <peter-b-n@hotmail.com>
2017-09-13 01:11:15 +0200

    Better slider

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-slider',
@@ -7,76 +7,30 @@ import { Router } from '@angular/router'
   styleUrls: ['./slider.component.scss']
 })
 export class SliderComponent implements OnInit {
-  currentIndex;
-  slideContainer;
-  indicators;
+  currentSlide = 1;
+  totalSlides = 2;
   progress = 0;
   constructor(private router: Router) { }
   ngOnInit () {
     if (this.router.url === '/') {
-        setInterval(() => {
-            this.progress += 1;
-            if (this.progress >= 100) {
-                this.nextSlide();
-                this.progress = 0;
-            }
-        }, 100);
+      setInterval(() => {
+        this.progress += 1;
+        if (this.progress >= 100) {
+          this.nextSlide();
+          this.progress = 0;
+        }
+      }, 100);
     }
   }
-  findCurrentSlide() {
+  nextSlide() {
+    (this.currentSlide !== this.totalSlides) ? this.currentSlide++ : this.currentSlide = 1;
     this.progress = 0;
-    this.slideContainer = document.getElementById('slide-container');
-    for (let i = 0; i < this.slideContainer.children.length; i++) {
-      if (this.slideContainer.children[i].className === 'slide current') {
-        this.currentIndex = i;
-      }
-    }
-    this.indicators = document.getElementById('indicators');
-    for (let i = 0; i < this.indicators.children.length; i++) {
-      if (this.indicators.children[i].className === 'indicator current') {
-        this.currentIndex = i;
-      }
-    }
-
-    for (let i = 0; i < this.slideContainer.children.length; i++) {
-      this.slideContainer.children[i].classList.remove('current');
-    }
-    for (let i = 0; i < this.indicators.children.length; i++) {
-      this.indicators.children[i].classList.remove('current');
-    }
   }
-  nextSlide () {
-    this.findCurrentSlide();
-    const current = this.currentIndex;
-    const childrenCount = this.slideContainer.children.length;
-    if (current + 1 === childrenCount) {
-      this.slideContainer.children[0].className += ' current';
-      this.indicators.children[0].className += ' current';
-    } else {
-      this.slideContainer.children[current + 1].className += ' current';
-      this.indicators.children[current + 1].className += ' current';
-    }
+  prevSlide() {
+      (this.currentSlide !== 1) ? this.currentSlide-- : this.currentSlide = this.totalSlides;
+      this.progress = 0;
   }
-  prevSlide () {
-    this.findCurrentSlide();
-    const current = this.currentIndex;
-    const childrenCount = this.slideContainer.children.length;
-    if (current === 0) {
-      this.slideContainer.children[this.slideContainer.children.length].className += ' current';
-    } else {
-      this.slideContainer.children[current - 1].className += ' current';
-    }
-  }
-  changeToThisSlide(event) {
-    this.findCurrentSlide();
-    let localIndex;
-    event.target.className += ' current';
-    for (let i = 0; i < this.indicators.children.length; i++) {
-      if (this.indicators.children[i].className === 'indicator current') {
-        localIndex = i;
-      }
-    }
-    this.slideContainer.children[localIndex].className += ' current';
-
+  changeSlide(id) {
+    this.currentSlide = id;
   }
 }
